e031ff8055a872610c169859200688aacac6f514
Student <student@Makerss-MacBook-Pro-8.local>
2019-10-23 15:31:09 +0100

    test added to check that quality is not increased above 50

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -34,6 +34,11 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq(6)
     end
+    it 'never increases the quality above 50' do
+      items = [Item.new("Aged Brie", 3, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(50)
+    end
   end
 
 end
